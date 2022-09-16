fa436a19e6787044b156247b2949d14d98b2324b
Connel Nwaodor <connel_n@hotmail.com>
2022-09-16 15:53:51 +0100

    Removed mages from banner

--- src/Components/BottomBanner/BottomBanner.jsx
+++ src/Components/BottomBanner/BottomBanner.jsx
@@ -1,13 +1,11 @@
 import "./BottomBanner.scss"
-import bannerRight from "../../assets/images/BDG_1920x800.webp"
-import bannerLeft from "../../assets/images/AB.webp"
+
 import React from 'react'
 
 const BottomBanner = () => {
   return (
     <>
-    <img className="banner-image" src={bannerRight} alt="" />
-    <img className="banner-image" src={bannerLeft} alt="" />
+
     </>
   )
 }
