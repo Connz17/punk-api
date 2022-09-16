ef1caaf5b7f11dc857b1ed434ce2b73f8a13bc02
Connel Nwaodor <connel_n@hotmail.com>
2022-09-16 15:51:18 +0100

    Changed bottom banner

--- src/Components/BottomBanner/BottomBanner.jsx
+++ src/Components/BottomBanner/BottomBanner.jsx
@@ -1,7 +1,6 @@
 import "./BottomBanner.scss"
 import bannerRight from "../../assets/images/BDG_1920x800.webp"
 import bannerLeft from "../../assets/images/AB.webp"
-
 import React from 'react'
 
 const BottomBanner = () => {
